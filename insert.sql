5f6f5ff1166a5bdfea8779ea9701eb442f2e3662
Devassine <a.devassine@orange.fr>
2017-06-03 12:06:04 +0200

    insert bdd

--- insert.sql
+++ insert.sql
@@ -1,20 +1,20 @@
 insert into city
-	values ('amsterdam', 'Netherlands', 'is the capital and most populous municipality of the Kingdom of the Netherlands.','https://www.hftp.org/hitec/amsterdam/i/SITE_160726_12581186_BQK12/content/CMS_07262016_131614000_L33KL/680137FB-0B39-584A-4AEF675BE3B3A60F.JPG');
+	values ('amsterdam', 'Netherlands', 'is the capital and most populous municipality of the Kingdom of the Netherlands.','https://lonelyplanetimages.imgix.net/mastheads/stock-photo-beautiful-amsterdam-76796579.jpg?sharp=10&vib=20&w=1200');
 
 insert into city
-	values ('athens', 'Greece', 'is the capital and largest city of Greece.','http://img.ev.mu/images/villes/395/1605x642/395.jpg' );
+	values ('athens', 'Greece', 'is the capital and largest city of Greece.','https://www.lonelyplanet.fr/sites/lonelyplanet/files/styles/manual_crop/public/media/destination/slider/descktop/gettyrf_88786323.jpg?itok=7mmXpPX4' );
 
 insert into city
-	values ('barcelona', 'Spain', 'is the capital city of the autonomous community of Catalonia in the Kingdom of Spain','https://visiter-barcelone.com/wp-content/uploads/2014/12/Panoramic-Barcelona.jpg');
+	values ('barcelona', 'Spain', 'is the capital city of the autonomous community of Catalonia in the Kingdom of Spain','https://www.transat.com/getmedia/805de2a9-6202-491e-bb9f-cdf57e3a248c/Barcelona-Madrid-002?width=579');
 
 insert into city
-	values ('paris', 'France', 'is the capital of France and its a city, with a population of 12 000 000 as of 2017.','http://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/France/Paris/paris-attractions-xlarge.jpg');
+	values ('paris', 'France', 'is the capital of France and its a city, with a population of 12 000 000 as of 2017.','https://reservation.parisinfo.com/InfoliveImages/cityvision/tour_eiffel/office_du_tourisme_de_paris_tour_eiffel_2.jpg?mode=crop&width=420&height=270&scale=both');
 
 insert into city
-	values ('lille', 'France', 'is at the north of France.','https://hautsdefrance.cci.fr/content/uploads/sites/6/2016/12/une-ccigrandlille.jpg');
+	values ('lille', 'France', 'is at the north of France.','http://france3-regions.francetvinfo.fr/hauts-de-france/sites/regions_france3/files/styles/top_big/public/assets/images/2017/02/27/27642144860_2b3efe9f89_k-2922225.jpg?itok=s6rhYEz5');
 
 insert into city
-	values ('belfast', 'Northern Ireland', ' is the capital and largest city of Northern Ireland, and the second largest on the island of Ireland.','http://visitbelfast.com/assets/gallery/generic/16201_Belfast_City_Hall_2x1.jpg');
+	values ('belfast', 'Northern Ireland', ' is the capital and largest city of Northern Ireland, and the second largest on the island of Ireland.','https://discovernorthernireland.com/contentassets/f156ee52774c483c8f9666411b28e441/walking-tours-of-belfast--n2.-ireland');
 
 insert into city
 	values ('cardiff', 'Wales', 'is the capital and largest city in Wales and the eleventh-largest city in the United Kingdom','http://www.chambery-airport.com/sites/chambery.aeroport.fr/files/styles/airport_pop_up/public/gallery/destination/galler-gezilecek-yerler.jpg?itok=lceRDgo9');
@@ -23,16 +23,10 @@ insert into city
 	values ('cologne', 'Germany', ' is the largest city in the German federal State of North Rhine-Westphalia and the fourth-largest city in Germany','http://www.cologne.de/sites/default/files/styles/lead/public/images/leads/skyline-nacht-900x300.jpg?itok=RXc5cg5N');
 
 insert into city
-	values ('dortmund', 'Germany', 'is an independent city in North Rhine-Westphalia, Germany.','https://www.sixt.fr/fileadmin/user_upload/pictures-city-page/dortmund-citypage-1.jpg');
-
-insert into city
-	values ('dublin', 'Ireland', ' is the capital and largest city of Ireland.','http://d5qsyj6vaeh11.cloudfront.net/images/destinations/dublin/dublin-main/ten-attractions/dublin-top-ten-hapenny-bridge-bg.jpg');
-
-insert into city
 	values ('florence', 'Italy', ' is the capital city of the Italian region of Tuscany and of the Metropolitan City of Florence.','https://www.lonelyplanet.fr/sites/lonelyplanet/files/styles/manual_crop/public/media/destination/slider/mobile/shutterstock_230424784.jpg?itok=VLdAOrtl');
 
 insert into city
-	values ('vilnius', 'Lithuania', 'is the capital of Lithuania and its largest city, with a population of 542,664 as of 2015.','https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Vilnius_St_Anns_church.jpg/220px-Vilnius_St_Anns_church.jpg');
+	values ('vilnius', 'Lithuania', 'is the capital of Lithuania and its largest city, with a population of 542,664 as of 2015.','https://fr.aegeanair.com/-/media/destinations/vilnius/vilnius_final_s_3.jpg?w=364&centercrop=1&h=224&usecustomfunctions=1&la=fr&hash=38200A8DA59D90FE7B5F89047D1A58A1FE9DE0A3');
 
 insert into client
 	values('Devassine','Alexandre','France','a.devassine@orange.fr','alex09011996');
